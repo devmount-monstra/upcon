606b267367919203facf92931df9c4bdd6b640e4
Andreas Müller <mail@devmount.de>
2016-04-27 20:53:13 +0200

    added: person fields

--- install/upcon.install.php
+++ install/upcon.install.php
@@ -4,30 +4,28 @@
 defined('MONSTRA_ACCESS') or die('No direct script access.');
 
 // Initialize Database
-Table::create('events', array('title'));
-$e = new Table('events');
-$e->addField('timestamp');
-$e->addField('deleted');
-$e->addField('category');
-$e->addField('date');
-$e->addField('openat');
-$e->addField('time');
-$e->addField('location');
-$e->addField('address');
-$e->addField('short');
-$e->addField('description');
-$e->addField('hashtag');
-$e->addField('facebook');
-$e->addField('image');
-$e->addField('imagesection');
-$e->addField('audio');
-$e->addField('color');
-
-Table::create('categories', array('title'));
-$c = new Table('categories');
-$c->addField('deleted');
-$c->addField('color');
+Table::create(
+	'upcon_persons',
+	array(
+		'prename',
+		'lastname',
+		'gender',
+		'birthday',
+		'email',
+		'address',
+		'zip',
+		'city',
+		'country',
+		'mobile',
+		'status',
+		'youth',
+		'safecom_visited',
+		'arrival',
+		'message',
+		'terms_accepted',
+	)
+);
 
 // Add Options
-Option::add('events_image_directory', '/');
-Option::add('events_audio_directory', '/');
+// Option::add('events_image_directory', '/');
+// Option::add('events_audio_directory', '/');
--- install/upcon.uninstall.php
+++ install/upcon.uninstall.php
@@ -1,12 +1,11 @@
-<?php 
+<?php
 
 // let only monstra allow to use this script
 defined('MONSTRA_ACCESS') or die('No direct script access.');
 
 // drop db tables
-// Table::drop('events');
-// Table::drop('categories');
+// Table::drop('upcon_persons');
 
 // Delete Options
-Option::delete('events_image_directory');
-Option::delete('events_audio_directory');
+// Option::delete('events_image_directory');
+// Option::delete('events_audio_directory');
