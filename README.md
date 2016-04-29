a8e344a4aa75c337bab23e874b005de7a0658590
Andreas Müller <mail@devmount.de>
2016-04-29 23:30:30 +0200

    in progress: main functions

--- README.md
+++ README.md
@@ -3,6 +3,11 @@ UPcon
 
 A plugin to provide a form for registration, mail address confirmationm, registration confirmation for UPcon, a convention organized by UPdate.
 
+## Version
+#### [**v1.0**](https://github.com/devmount-monstra/upcon/releases/tag/v1.0) - 2016-04-29
+
+Here is a [list of all releases](https://github.com/devmount-monstra/upcon/releases) of `UPcon`
+
 ## Installation
 1. Download latest version of `UPcon`
 2. Log into Monstra backend and go to `Content > Plugins > Install new`
@@ -10,23 +15,26 @@ A plugin to provide a form for registration, mail address confirmationm, registr
 5. Go to `Content > UPcon > Configuration`, set your configuration and click `Save`
 6. Now everything is ready to use!
 
-## Frontend
+## Usage
 Shortcode for content pages:
 
-    {upcon type="..." time="..." order="..."}
+    {upcon show="registration" title="UPcon 2016" id="upcon16"}
 
 Codesnippet for templates:
 
-    <?php upcon::show(<...>, <...>); ?>
+    <?php UPcon::registration('The answer to life, the universe and everything?', '42'); ?>
 
-## Configuration
+## Options
+| option         | type     | description                                                 |
+|----------------|----------|-------------------------------------------------------------|
+| Slide duration | `int`    | How many milliseconds the sliding animation should take     |
+| Slide easing   | `string` | Easing type for the sliding animation: `swing` or `linear`  |
 
-| field           | description                                                                                                  |
-|-----------------|--------------------------------------------------------------------------------------------------------------|
-| Image directory | directory for event images. Those images will be displayed in the select list of the upcon add/edit formula. |
-  
 ## License
 This Plugin is distributed under [MIT-License](http://opensource.org/licenses/mit-license.html).
 
 ## Sources
-This plugin is developed by [devmount](http://devmount.de).
+No external sources in use.
+
+---
+created by [devmount ![devmount logo](http://media.devmount.de/devmount_small_dark.png)](http://devmount.de)
--- upcon.plugin.php
+++ upcon.plugin.php
@@ -4,64 +4,101 @@
 defined('MONSTRA_ACCESS') or die('No direct script access.');
 
 /**
- *	Register plugin
+ *	UPcon plugin
  *
- *  Provides forms for registration
+ *  A plugin to provide a form for registration, mail address confirmationm,
+ *  registration confirmation for UPcon, a convention organized by UPdate.
  *
  *	@package    Monstra
  *  @subpackage Plugins
  *	@author     Andreas Müller | devmount <mail@devmount.de>
  *	@license    MIT
- *	@version    0.1.2016-01-02
- *  @link       https://github.com/devmount-monstra/register
+ *	@version    1.0
+ *  @link       https://github.com/devmount-monstra/upcon
  *
  */
 
 
-// Register plugin
+// UPcon plugin
 Plugin::register(
     __FILE__,
-    __('Register'),
-    __('Form registration management for Monstra.'),
-    '0.1.2016-01-02',
+    __('UPcon', 'upcon'),
+    __('Form registration for UPcon events.', 'upcon'),
+    '1.0',
     'devmount',
     'http://devmount.de'
 );
 
 // Include plugin admin
 if (Session::exists('user_role') && in_array(Session::get('user_role'), array('admin', 'editor'))) {
-    Plugin::Admin('register');
+    Plugin::Admin('upcon');
 }
 
 
 /**
  * Add shortcode
  */
-Shortcode::add('register', 'Register::_shortcode');
+Shortcode::add('upcon', 'UPcon::_shortcode');
 
 
 /**
  * Add CSS and JavaScript
  */
-Action::add('theme_footer', 'Register::_insertJS');
-Action::add('theme_header', 'Register::_insertCSS');
+Action::add('theme_footer', 'UPcon::_insertJS');
+Action::add('theme_header', 'UPcon::_insertCSS');
 
 
 /**
- * Register class
+ * UPcon class
  *
  * <code>
- *      <?php Register::show('list', 'minimal', 'future', 5, 'ASC'); ?>
+ *      <?php UPcon::show('list', 'minimal', 'future', 5, 'ASC'); ?>
  * </code>
  *
  */
-class Register
+class UPcon
 {
+    /**
+     * Participant status: Normal
+     *
+     * @var integer
+     */
+    const STATUS_NORMAL = 1;
+
+    /**
+     * Participant status: Early
+     *
+     * @var integer
+     */
+    const STATUS_EARLY = 2;
+
+    /**
+     * Participant status: Buju
+     *
+     * @var integer
+     */
+    const STATUS_BUJU = 3;
+
+    /**
+     * Participant status: Staff
+     *
+     * @var integer
+     */
+    const STATUS_STAFF = 4;
+
+    /**
+     * Participant status: Visitor
+     *
+     * @var integer
+     */
+    const STATUS_VISITOR = 5;
+
+
     /**
      * Creates shortcodes for content pages
      *
      * <code>
-     *      {register type="list" style="minimal" time="future" count="5" order="ASC"}
+     *      {upcon show="registration" title="UPcon 2016" id="upcon16"}
      * </code>
      *
      * @param  array $attributes given
@@ -70,17 +107,17 @@ class Register
      */
     public static function _shortcode($attributes)
     {
-        switch ($attributes['type']) {
-            case 'list':
-                return Register::listRegister($attributes['style'], $attributes['time'], $attributes['count'], $attributes['order']);
+        switch ($attributes['show']) {
+            case 'registration':
+                return UPcon::registration($attributes['id'], $attributes['title']);
                 // return 'test';
                 break;
 
             default:
-                return Register::error();
+                return UPcon::error();
                 break;
         }
-        return Register::error();
+        return UPcon::error();
     }
 
 
@@ -104,28 +141,35 @@ class Register
      */
     public static function _insertCSS()
     {
-        echo '<link rel="stylesheet" type="text/css" href="' . Option::get('siteurl') . '/plugins/register/css/register.plugin.css" />';
+        echo '<link rel="stylesheet" type="text/css" href="' . Option::get('siteurl') . '/plugins/upcon/css/upcon.plugin.css" />';
     }
 
 
     /**
      * Assign to view
+     *
+     * @param  int     $id     of current registration set
+     * @param  string  $title  of the UPcon
      */
-    public function listRegister($style, $time = 'all', $count = 'all', $order = 'ASC')
+    public function registration($id, $title)
     {
-        // handle style
-        $template = '';
-        if (in_array(trim($style), array('extended', 'minimal'))) {
-            $template = 'list-' . trim($style);
-        } else {
-            $template = 'list-minimal';
-        }
-
-        return View::factory('register/views/frontend/' . $template)
-            ->assign('eventlist', Register::_getRegister($time, $count, $order))
-            ->assign('categories', array(
-                'color' => Register::_getCategoryAttributes('color'),
-                'title' => Register::_getCategoryAttributes('title'),
+        return View::factory('upcon/views/frontend/registration')
+            ->assign('title', $title)
+            ->assign('id', $id)
+            ->assign('decision', array(
+                0 => __('Nein', 'upcon'),
+                1 => __('Ja', 'upcon'),
+            ))
+            ->assign('gender', array(
+                'f' => __('weiblich', 'upcon'),
+                'm' => __('männlich', 'upcon'),
+            ))
+            ->assign('status', array(
+                UPcon::STATUS_NORMAL => __('Teilnehmer', 'upcon'),
+                // UPcon::STATUS_EARLY => __('Frühbucher', 'upcon'),
+                // UPcon::STATUS_BUJU => __('BUJU', 'upcon'),
+                UPcon::STATUS_STAFF => __('Mitarbeiter', 'upcon'),
+                UPcon::STATUS_VISITOR => __('Tagesgast', 'upcon'),
             ))
             ->render();
     }
